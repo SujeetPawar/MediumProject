9bda7d87157993c4f08971fa2f3dcb4ad0d50cd1
SujeetPawar <92884614+SujeetPawar@users.noreply.github.com>
2024-04-16 18:07:21 +0530

    Update Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({ id: id || "" });
-  if (loading) {
+  if (loading || !blog) {
     return (
       <div>
         <Appbar />
