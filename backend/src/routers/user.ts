9927a8175019ba37c98dff92b4d2e8e70a06f5f2
sujeet <sujeet17052003@gmail.com>
2024-04-16 17:46:13 +0530

    added all the files

---
+++ backend/src/routers/user.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { sign, verify } from "hono/jwt";
+import { signInInput, signUpInput } from "@sujeet_17/common";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const { success } = signUpInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      msg: "Inputs are incorrects",
+    });
+  }
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name:body.name,
+        username: body.username,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const { success } = signInInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      msg: "Inputs are incorrects",
+    });
+  }
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" });
+    }
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+});
